9571db329c1754cef845f4695df6881bf199fe13
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2014-07-18 11:33:06 +0900

    Add receive_message

--- chat/chatd.c
+++ chat/chatd.c
@@ -12,47 +12,6 @@
 const short PORT = 8090;
 const int LISTENQ = 10;
 
-struct sockaddr_in create_server_addr(short port) {
-    struct sockaddr_in addr = {
-        .sin_family = AF_INET,
-        .sin_addr = {
-            .s_addr = htonl(INADDR_ANY)
-        },
-        .sin_port = htons(port)
-    };
-    return addr;
-}
-
-int create_listened_socket(struct sockaddr_in addr) {
-    int sockfd = safe_socket(AF_INET, SOCK_STREAM, 0);
-    int reuse = 1;
-    safe_setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (const void *) &reuse, sizeof(reuse));
-    safe_bind(sockfd, (struct sockaddr *) &addr, sizeof(addr));
-    safe_listen(sockfd, LISTENQ);
-    return sockfd;
-}
-
-void send_message(int epoll_fd, user_t receiver) {
-    message_t message = pop_message(receiver->queue);
-    send(receiver->fd, message->body, strlen(message->body), 0);
-    delete_message(message);
-
-    if (receiver->queue->n_of_messages == 0) {
-        safe_epoll_ctl1(epoll_fd, EPOLL_CTL_MOD, receiver->fd, EPOLLIN);
-    }
-}
-
-void enqueue_message_to_others(int epoll_fd, user_manager_t manager, user_t sender, const char *body) {
-    for (int i = 0; i < manager->n_of_users; i++) {
-        user_t user = manager->users[i];
-        if (user == sender) {
-            continue;
-        }
-        push_message(user->queue, sender->name, body);
-        safe_epoll_ctl1(epoll_fd, EPOLL_CTL_MOD, user->fd, EPOLLIN | EPOLLOUT);
-    }
-}
-
 int main(int argc, char *argv[]) {
     struct sockaddr_in server_addr = create_server_addr(PORT);
     int listen_fd = create_listened_socket(server_addr);
@@ -86,24 +45,68 @@ int main(int argc, char *argv[]) {
             if (events[i].events & EPOLLOUT) {
                 send_message(epoll_fd, user);
             } else if (events[i].events & EPOLLIN) {
-                char buffer[1024];
-                memset(buffer, 0, sizeof(buffer));
-                int ret = read(fd, buffer, sizeof(buffer));
-                if (ret == -1) {
-                    perror("read(2)");
-                    exit(EXIT_FAILURE);
-                } else if (ret == 0) {
-                    /* connection was closed by remote host */
-                    printf("user %s left.\n", user->name);
-                    safe_epoll_ctl(epoll_fd, EPOLL_CTL_DEL, fd, NULL);
-                    delete_user_by_fd(manager, fd);
-                    continue;
-                }
-                printf("%s:%s", user->name, buffer);
-
-                enqueue_message_to_others(epoll_fd, manager, user, buffer);
+                receive_message(epoll_fd, manager, user);
             }
         }
     }
     return 0;
+}
+
+struct sockaddr_in create_server_addr(short port) {
+    struct sockaddr_in addr = {
+        .sin_family = AF_INET,
+        .sin_addr = {
+            .s_addr = htonl(INADDR_ANY)
+        },
+        .sin_port = htons(port)
+    };
+    return addr;
+}
+
+int create_listened_socket(struct sockaddr_in addr) {
+    int sockfd = safe_socket(AF_INET, SOCK_STREAM, 0);
+    int reuse = 1;
+    safe_setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (const void *) &reuse, sizeof(reuse));
+    safe_bind(sockfd, (struct sockaddr *) &addr, sizeof(addr));
+    safe_listen(sockfd, LISTENQ);
+    return sockfd;
+}
+
+void send_message(int epoll_fd, user_t receiver) {
+    message_t message = pop_message(receiver->queue);
+    send(receiver->fd, message->body, strlen(message->body), 0);
+    delete_message(message);
+
+    if (receiver->queue->n_of_messages == 0) {
+        safe_epoll_ctl1(epoll_fd, EPOLL_CTL_MOD, receiver->fd, EPOLLIN);
+    }
+}
+
+void receive_message(int epoll_fd, user_manager_t manager, user_t sender) {
+    char buffer[1024];
+    memset(buffer, 0, sizeof(buffer));
+    int ret = read(sender->fd, buffer, sizeof(buffer));
+    if (ret == -1) {
+        perror("read(2)");
+        exit(EXIT_FAILURE);
+    } else if (ret == 0) {
+        /* connection was closed by remote host */
+        printf("user %s left.\n", sender->name);
+        safe_epoll_ctl(epoll_fd, EPOLL_CTL_DEL, sender->fd, NULL);
+        delete_user_by_fd(manager, sender->fd);
+        return;
+    }
+    printf("%s:%s", sender->name, buffer);
+    enqueue_message_to_others(epoll_fd, manager, sender, buffer);
+}
+
+void enqueue_message_to_others(int epoll_fd, user_manager_t manager, user_t sender, const char *body) {
+    for (int i = 0; i < manager->n_of_users; i++) {
+        user_t user = manager->users[i];
+        if (user == sender) {
+            continue;
+        }
+        push_message(user->queue, sender->name, body);
+        safe_epoll_ctl1(epoll_fd, EPOLL_CTL_MOD, user->fd, EPOLLIN | EPOLLOUT);
+    }
 }
--- chat/chatd.h
+++ chat/chatd.h
@@ -1,4 +1,13 @@
 #ifndef CHATD_H
 #define CHATD_H
 
+#include "user_manager.h"
+
+struct sockaddr_in create_server_addr(short port);
+int create_listened_socket(struct sockaddr_in addr);
+
+void send_message(int epoll_fd, user_t receiver);
+void receive_message(int epoll_fd, user_manager_t manager, user_t sender);
+void enqueue_message_to_others(int epoll_fd, user_manager_t manager, user_t sender, const char *body);
+
 #endif /* CHATD_H */
